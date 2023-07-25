e2788a4c16c30571ecc63b55910cae1eea1d9a33
root <ayegboyinyinka4332@gmail.com>
2023-07-25 11:07:16 +0000

    My commit

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,25 @@
+export default function iterateThroughObject(reportWithIterator) {
+
+ }
+
+{
+  allEmployees: {
+     engineering: [
+          'John Doe',
+          'Guillaume Salva',
+     ],
+  },
+  ...
+};
+
+
+> employees = {
+      ...createEmployeesObject('engineering', engineering),
+      ...createEmployeesObject('design', design),
+    };
+>
+> const report = createReportObject(employees);
+> const reportWithIterator = createIteratorObject(report);
+> iterateThroughObject(reportWithIterator)
+'John Doe | Guillaume Salva | Kanye East | Jay Li'
+> 
